62677e8cbd7e8a611260fd280c1f1682cb9fd4ac
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-12-11 16:03:28 -0800

    revised ball animation to remove double image rendering bug and animated right paddle (computer paddle)

--- frontend/src/components/Canvas.jsx
+++ frontend/src/components/Canvas.jsx
@@ -4,28 +4,23 @@ import './styles/Canvas.css';
 
 const Canvas = function(props) {
   // const {} = props;
-  const fps = 30;
+  // const fps = 30;
   const canvasRef = useRef(null);
-  const ballRef = useRef({x: 700, y: 400, r: 10})
-  var ballSpeedX = 10;
-  var ballSpeedY = 10;
-  
-  const renderAll = (context, ball) => {
-    createBoard(context);
+  const ballRef = useRef({x: 700, y: 400, r: 10});
+  const paddleRRef = useRef({x: 1360, y: 320, w: 11.2, h:136 })
+  let ballSpeedX = 15;
+  let ballSpeedY = 7;
+  let paddleSpeedY = 7;
+  console.log(paddleRRef)
+
+  const createBoard = (context, ball, paddle) => {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    
     //ball
-    context.save();
     context.beginPath(); //general routine for howd you fill any shape
     context.fillStyle = 'white';
     context.arc(ball.x, ball.y, ball.r, 0, 2*Math.PI, true);
     context.fill(); //general routine for howd you fill any shape
-    context.closePath();
-    context.restore();
-  };
-
-  const createBoard = (context) => {
-    //board
-    context.fillStyle = 'black';
-    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
 
     //paddle 1
     context.fillStyle = 'white';
@@ -34,40 +29,59 @@ const Canvas = function(props) {
 
     //paddle 2
     context.fillStyle = 'white';
-    context.fillRect((context.canvas.width - 35), (context.canvas.height * .40), (context.canvas.width * .008), (context.canvas.height * .17));
+    context.fillRect(paddle.x, paddle.y, paddle.w, paddle.h);
     context.fill();
-
+  
   };
 
-  const updateAll = function(context) {
-    setInterval(function() {
-      ballRef.current.x += ballSpeedX;
-      ballRef.current.y += ballSpeedY;
+  const updateBall = function(context, ball) {
+    ball.x += ballSpeedX;
+    ball.y += ballSpeedY;
 
-      if (ballRef.current.x < 0) {
-        ballSpeedX *= -1;
-      }
-      if (ballRef.current.x > context.canvas.width) {
-        ballSpeedX *= -1;
-      }
+    if (ball.x - ball.r  <= 0) {
+      ballSpeedX *= -1;
+      ball.x = ball.r;
+    }
 
-      if (ballRef.current.y < 0) {
-        ballSpeedY *= -1;
-      }
-      if (ballRef.current.y > context.canvas.height) {
-        ballSpeedY *= -1;
-      }
+    if (ball.x + ball.r >= context.canvas.width) {
+      ballSpeedX *= -1;
+      ball.x = context.canvas.width - ball.r;
+    }
 
-      renderAll(context, ballRef.current);
-    }, 1000/fps);
+    if (ball.y - ball.r  <= 0) {
+      ballSpeedY *= -1;
+      ball.y = ball.r;
+    }
+    if (ball.y + ball.r >= context.canvas.height) {
+      ballSpeedY *= -1;
+      ball.y = context.canvas.height - ball.r;
+    }
   };
 
-  useEffect(()=> { //need a useEffect to control/trigger side effects for our components. We want to call/use this code after our Canvas component is rendered -> useEffect allows for this
+  const updatePaddleR = function(context, paddle) {
+    paddle.y += paddleSpeedY;
+
+    if (paddle.y >= context.canvas.height-136) {
+      paddleSpeedY *= -1;
+    }
+
+    if (paddle.y <= 0) {
+      paddleSpeedY *= -1;
+    }
+  };
+
+  const render = () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d'); //obtains the rendering context and its drawing functions
-    
-    createBoard(context)
-    updateAll(context);
+    updateBall(context, ballRef.current);
+    updatePaddleR(context, paddleRRef.current);
+    createBoard(context, ballRef.current, paddleRRef.current);
+    requestAnimationFrame(render);
+  }
+
+  useEffect(()=> { //need a useEffect to control/trigger side effects for our components. We want to call/use this code after our Canvas component is rendered -> useEffect allows for this
+
+    render();
 
   }, []) //empty array says we only want to trigger this function once
 

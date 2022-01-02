c54f8955baa985930a71a1672faa505ffa181db8
Amaneet Hundal <amanhundal19@gmail.com>
2022-01-02 15:01:29 -0800

    added images for WIP game

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,8 +9,8 @@ const [state, setState] = useState({
   users: [],
   scoreP1: 0,
   scoreP2: 0,
-  minutes: 2,
-  seconds: 30
+  minutes: 1,
+  seconds: 0
 });
 
 const increaseScoreP1 = () => {
@@ -26,7 +26,7 @@ const increaseScoreP2 = () => {
 const animateTimer = () => {
   let timerId = setInterval( () => {
     if (state.seconds === 0 && state.minutes === 0) {
-      clearInterval(timerId)
+      clearInterval(timerId);
     } else if (state.seconds === 0) {
       setState({...state, seconds: state.seconds = 59});
       setState({...state, minutes: state.minutes -= 1});
@@ -37,7 +37,7 @@ const animateTimer = () => {
 }
 
 //testdata
-const testData = [{name: "Amo", score: 50}, {name:"Elliot", score: 100}]
+const testData = [{name: "Amo", score: 50}, {name:"Elliot", score: 100}];
 
   return (
     <div className="App">

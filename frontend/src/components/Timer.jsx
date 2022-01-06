a17596b53558625eb0d61388d0de490901a78234
Amaneet Hundal <amanhundal19@gmail.com>
2022-01-05 18:31:09 -0800

    added endgame logic for timer and score

--- frontend/src/components/Timer.jsx
+++ frontend/src/components/Timer.jsx
@@ -3,11 +3,6 @@ import {useEffect} from 'react';
 const Timer = (props) => {
   const {mins, secs, animateTimer} = props;
   
-  // useEffect(() => {
-  //   animateTimer();
-
-  // }, [])
-  
   return (
     <h1>{mins}:{secs < 10 ? "0" : ""}{secs}</h1>
   )

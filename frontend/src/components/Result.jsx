4b2c926b7830f01fd84c9391b9a7c296c489d795
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2022-01-10 17:58:02 -0800

    added popup component used to show game result

---
+++ frontend/src/components/Result.jsx
@@ -0,0 +1,13 @@
+const Result = function(props) {
+  // const { winner } = props;
+  return (
+    <div className="popup">
+      <div className="inner-popup">
+        <h1>Winner/Draw</h1>
+        <button>Close</button>
+      </div>
+    </div>
+  )
+ }
+
+ export default Result;

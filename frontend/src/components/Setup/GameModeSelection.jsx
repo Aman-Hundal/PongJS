73fe5bdbd54bd3e38813df9cc32ecae8528c69d3
Amaneet Hundal <amanhundal19@gmail.com>
2022-01-15 16:59:15 -0800

    font added

--- frontend/src/components/Setup/GameModeSelection.jsx
+++ frontend/src/components/Setup/GameModeSelection.jsx
@@ -1,4 +1,4 @@
-import '../styles/GameModeSelection.css';
+import '../styles/Setup.css';
 import Button from './Button';
 
 const GameModeSelection = (props) => {
@@ -7,7 +7,8 @@ const GameModeSelection = (props) => {
         <div className="gamemode-card">
             <h2>Select a Game Mode: </h2>
             <ul id="gamemode-list">
-                <li><Button message={"Online"} onClick={() => transition("CONFIRMP1")}/></li>
+                <li onClick={() => transition("CONFIRMP1")}>Online</li>
+                <br></br>
                 <li>Offline (WIP)</li>
             </ul>
         </div>

16fd60a9c06d30aa4b724461a4377a83400dd2c5
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2022-01-18 15:13:34 -0800

    setup env file

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const matches = require('./routes/matches');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); 
 const port = process.env.PORT || 3001;
 
 //DB setup
-// mongoose.connect('mongodb://localhost/matches', {useNewUrlParser: true}); //connects to mongo DB. Mongoose is an express/node.js package that allows for ease use between mongo db and node/express
+// mongoose.connect('mongodb://localhost/matches', {useNewUrlParser: true}); //connects to mongo DB. Mongoose is an express/node.js package that allows for ease use between mongo db and node/express. ITS AN ORM
 // const db = mongoose.connection;
 // db.on('error', (error) => console.log(error));
 // db.once('open', () => console.log("Connected to database"));
